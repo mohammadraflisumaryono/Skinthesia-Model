472e41e860ffccf184c6deaadf84c512b47f5854
mohammadraflisumaryono <mohammadraflisumaryono@gmail.com>
2025-05-18 18:14:28 +0700

    Enhance product scraping functionality and save results to CSV

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils.extracts.category_scraper import get_categories
 from utils.extracts.product_scraper import get_products_from_category
+import pandas as pd
 import os
 
 def setup_logger():
@@ -30,6 +31,7 @@ def run_scraper():
     driver = webdriver.Chrome(options=options)
 
     logging.info("Memulai scraping kategori...")
+    all_products = []
 
     try:
         categories = get_categories(limit=3)  # 3 kategori untuk percobaan
@@ -41,9 +43,19 @@ def run_scraper():
             logging.info(f" Jumlah produk ditemukan: {len(products)}")
             for i, prod in enumerate(products, 1):
                 logging.info(f"[{i}] {prod['brand']} - {prod['name']} |  {prod['rating']} ({prod['total_reviews']} reviews)")
+            all_products.extend(products)
     finally:
         driver.quit()
         logging.info("Scraping selesai dan browser ditutup.")
+         # Simpan ke CSV
+    if all_products:
+        df = pd.DataFrame(all_products)
+        if not os.path.exists("data"):
+            os.makedirs("data")
+        df.to_csv("data/raw.csv", index=False, encoding='utf-8-sig')
+        logging.info(f"Data berhasil disimpan ke data/raw.csv ({len(df)} baris)")
+    else:
+        logging.warning("Tidak ada data produk yang berhasil dikumpulkan.")
 
 if __name__ == "__main__":
     run_scraper()
